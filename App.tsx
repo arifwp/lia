4419e488a8cc1322355c9311fca978237cd45e09
Arif Wahyu <arifwahyu696@gmail.com>
2025-09-24 17:06:42 +0700

    add navigation and toast manager

--- App.tsx
+++ App.tsx
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  focusManager,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { AppStateStatus, Platform } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import ToastManager from "toastify-react-native";
+import { CustomToastConfig } from "./components/CustomToastConfig";
+import { useAppState } from "./hooks/useAppState";
+import { useOnlineManager } from "./hooks/useOnlineManager";
+import { LoginScreen } from "./screens/auth/LoginScreen";
+import { RegisterScreen } from "./screens/auth/RegisterScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const onAppStateChange = (status: AppStateStatus) => {
+  if (Platform.OS !== "web") {
+    focusManager.setFocused(status === "active");
+  }
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: 2 } },
+});
+
+export type RootStackParamList = {
+  Register: undefined;
+  Login: undefined;
+};
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "Register",
+  screenOptions: {
+    headerShown: false,
+  },
+  screens: {
+    Register: RegisterScreen,
+    Login: LoginScreen,
   },
 });
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  useOnlineManager();
+
+  useAppState(onAppStateChange);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ToastManager
+          position="bottom"
+          duration={3000}
+          config={CustomToastConfig}
+          showCloseIcon={true}
+          animationStyle={"slide"}
+          autoHide={true}
+        />
+
+        <Navigation />
+      </SafeAreaProvider>
+    </QueryClientProvider>
+  );
+}
