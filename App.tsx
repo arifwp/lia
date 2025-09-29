eafd2179c97269005dafad3088ddcf28a2a26d5f
Arif Wahyu <arifwahyu696@gmail.com>
2025-09-29 17:10:24 +0700

    setup fonts

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
 import { createStaticNavigation } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {
   focusManager,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { AppStateStatus, Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ToastManager from "toastify-react-native";
 import { CustomToastConfig } from "./components/CustomToastConfig";
 import { useAppState } from "./hooks/useAppState";
 import { useOnlineManager } from "./hooks/useOnlineManager";
-import { LoginScreen } from "./screens/auth/LoginScreen";
-import { RegisterScreen } from "./screens/auth/RegisterScreen";
+import { RootNav } from "./navs/RootNav";
 
 const onAppStateChange = (status: AppStateStatus) => {
   if (Platform.OS !== "web") {
@@ -24,28 +25,30 @@ const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 2 } },
 });
 
-export type RootStackParamList = {
-  Register: undefined;
-  Login: undefined;
-};
+const Navigation = createStaticNavigation(RootNav);
 
-const RootStack = createNativeStackNavigator({
-  initialRouteName: "Register",
-  screenOptions: {
-    headerShown: false,
-  },
-  screens: {
-    Register: RegisterScreen,
-    Login: LoginScreen,
-  },
-});
-
-const Navigation = createStaticNavigation(RootStack);
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   useOnlineManager();
 
   useAppState(onAppStateChange);
+
+  const [fontsLoaded] = useFonts({
+    "Sen-Regular": require("./assets/fonts/Sen-Regular.ttf"),
+    "Sen-Medium": require("./assets/fonts/Sen-Medium.ttf"),
+    "Sen-SemiBold": require("./assets/fonts/Sen-SemiBold.ttf"),
+    "Sen-Bold": require("./assets/fonts/Sen-Bold.ttf"),
+    "Sen-ExtraBold": require("./assets/fonts/Sen-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
 
   return (
     <QueryClientProvider client={queryClient}>
